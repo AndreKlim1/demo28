84734b2db7b16d79edc5e987a1625c47e3e86638
AndreKlim1 <andreyklimovich159@gmail.com>
2021-10-20 19:12:41 +0300

    Next step

--- src/main/java/com/example/demo28/HelloController.java
+++ src/main/java/com/example/demo28/HelloController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;fjbdsvbsdvbdvhsv
+    private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
